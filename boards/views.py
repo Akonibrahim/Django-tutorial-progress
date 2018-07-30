6f22b76da8958b84000c2cae823a177eb5bd3318
Akonibrahim <akonibrahim1@gmail.com>
2018-07-30 12:58:17 +0530

    After done reusable templates

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+# from django.http import Http404
 from .models import Board
+
 
 def home(response):
     boards = Board.objects.all()
     return render(response, 'home.html', {'boards':boards})
+
+def board_topics(response, id):
+    board = get_object_or_404(Board,id=id)
+    return render(response, 'topics.html', {'board' : board})
